8d934106d0350b8e06d06dd90c6796ee83a06b58
Glitch (quiver-safe-paneer) <none>
2025-05-12 00:38:50 +0000

    🎀🐬 Updated with Glitch

--- public/login.js
+++ public/login.js
@@ -30,6 +30,8 @@ async function login() {
 
   if (data.success) {
     localStorage.setItem("userId", data.user._id);
+    localStorage.setItem("userEmail", data.user.email);
+    localStorage.setItem("username", data.user.username); // o user.username
     window.location.href = "index.html";
   } else {
     response.textContent = data.message;
